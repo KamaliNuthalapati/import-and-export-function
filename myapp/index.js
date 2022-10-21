6da43207a9921c7d0be1d9cb710cf27e5bf4494f
KamaliNuthalapati <kamalinuthalapati@gmail.com>
2022-10-21 05:06:10 +0000

    Intializing Database Server

--- myapp/index.js
+++ myapp/index.js
@@ -1,4 +1,28 @@
 const express = require("express");
+const path = require("path");
+
+const {open} = require("sqlite");
+const sqlite3 = require("sqlite3");
 const app = express();
 
-app.listen(3000);
+
+const dbPath = path.join(__dirname, "goodreads.db");
+
+let db = null;
+
+const initializeDBAndServer = async () =>{
+    try{
+        db = await open({
+            filename: dbPath,
+            driver: sqlite3.Database,
+        });
+        app.listen(3000, () =>{
+            console.log("Server Running at http://localhost:3000/");
+        });
+    }catch(error){
+        console.log(`DB error:${error.message}`);
+        process.exit(1);
+    }
+};
+
+initializeDBAndServer();
