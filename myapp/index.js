d4bb0b66cd1a689b8e80310af6aac7e1eea216e7
KamaliNuthalapati <kamalinuthalapati@gmail.com>
2022-10-21 05:21:55 +0000

    add API call of get books

--- myapp/index.js
+++ myapp/index.js
@@ -1,28 +1,38 @@
 const express = require("express");
 const path = require("path");
 
-const {open} = require("sqlite");
+const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
 const app = express();
-
 
 const dbPath = path.join(__dirname, "goodreads.db");
 
 let db = null;
 
-const initializeDBAndServer = async () =>{
-    try{
-        db = await open({
-            filename: dbPath,
-            driver: sqlite3.Database,
-        });
-        app.listen(3000, () =>{
-            console.log("Server Running at http://localhost:3000/");
-        });
-    }catch(error){
-        console.log(`DB error:${error.message}`);
-        process.exit(1);
-    }
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3001, () => {
+      console.log("Server Running at http://localhost:3001/");
+    });
+  } catch (error) {
+    console.log(`DB error:${error.message}`);
+    process.exit(1);
+  }
 };
 
 initializeDBAndServer();
+
+app.get("/books/", async (request, response) => {
+  const getBooksQuery = `
+    SELECT 
+    *
+    FROM
+    book
+    ORDER BY book_id;`;
+  const bookArray = await db.all(getBooksQuery);
+  response.send(bookArray);
+});
